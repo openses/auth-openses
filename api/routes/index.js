62c50ac75fef8b40811c6bed28c743ad4234ad76
buerojacob <claude.jacob@buerojacob.ch>
2018-10-19 19:34:19 +0200

    ok

--- api/routes/index.js
+++ api/routes/index.js
@@ -12,6 +12,13 @@ var ctrlAuth = require('../controllers/authentication');
 // profile
 router.get('/profile', auth, ctrlProfile.profileRead);
 
+router.get('/profiletest', auth, ctrlProfile.profileRead);
+
+router.get('/.well-known/acme-challenge/a-string', function(req, res, next) {
+  console.log('Test');
+  res.render('Express Test a-string');
+});
+
 // authentication
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
