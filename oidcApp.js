a9be8d5baa4e8c26c845010150f163de86864c1e
claude.jacob@buerojacob.ch <claude.jacob@buerojacob.ch>
2019-03-01 06:10:29 +0100

    add client

--- oidcApp.js
+++ oidcApp.js
@@ -6,42 +6,57 @@ const oidcApp = express();
 const clients = [
     // token id_token code
     {client_id: 'test_oidcdebugger_com_token_id_token_code',
+    client_secret: 'super_secret',
     grant_types: ['authorization_code', 'implicit'],
     response_types: ['token id_token code'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'},
+    // local -> token id_token code
+    {client_id: 'local_test_oidcdebugger_com_token_id_token_code',
+    client_secret: 'super_secret',
+    grant_types: ['authorization_code', 'implicit'],
+    response_types: ['token id_token code'],
+    // redirect_uris: ['https://oidcdebugger.com/debug'],
+    redirect_uris: ['https://127.0.0.1:3001/callback'],
+    token_endpoint_auth_method: 'none'},
     // token code
     {client_id: 'test_oidcdebugger_com_token_code',
+    client_secret: 'client_secret',
     grant_types: ['authorization_code', 'implicit'],
     response_types: ['token code'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'},
     // code id_token 
     {client_id: 'test_oidcdebugger_com_code_id_token',
+    client_secret: 'client_secret',
     grant_types: ['authorization_code', 'implicit'],
     response_types: ['code id_token'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'}, 
     // token id_token
     {client_id: 'test_oidcdebugger_com_token_id_token',
+    client_secret: 'client_secret',
     grant_types: ['implicit'],
     response_types: ['token id_token'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'},
     // code
     {client_id: 'test_oidcdebugger_com_code',
+    client_secret: 'client_secret',
     grant_types: ['authorization_code'],
     response_types: ['code'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'},
     // token
     {client_id: 'test_oidcdebugger_com_token',
+    client_secret: 'client_secret',
     grant_types: ['implicit'],
     response_types: ['token'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
     token_endpoint_auth_method: 'none'},
     // id_token
     {client_id: 'test_oidcdebugger_com_id_token',
+    client_secret: 'client_secret',
     grant_types: ['implicit'],
     response_types: ['id_token'],
     redirect_uris: ['https://oidcdebugger.com/debug'],
@@ -49,14 +64,15 @@ const clients = [
     
     {
     client_id: 'test_oauth_app',
+    client_secret: 'client_secret',
     client_secret: 'super_secret',
     grant_types: ['client_credentials'],
     redirect_uris: [],
     response_types: [],
 }];
 
-const oidc = new Provider('http://auth-openses.westeurope.azurecontainer.io:3000', {
-// const oidc = new Provider('http://localhost:3000', {
+// const oidc = new Provider('http://auth-openses.westeurope.azurecontainer.io:3000', {
+const oidc = new Provider('http://localhost:3000', {
     claims: {
         address: ['address'],
         email: ['email', 'email_verified'],
